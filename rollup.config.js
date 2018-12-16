392524a256e56cea19348407c805e6907d84859f
wessberg <frederikwessberg@gmail.com>
2018-12-16 12:12:52 +0100

    Will output ES2017 rather than ESNExt

--- rollup.config.js
+++ rollup.config.js
@@ -25,6 +25,7 @@ export default {
 	external: [
 		...Object.keys(packageJSON.dependencies),
 		...Object.keys(packageJSON.devDependencies),
+		...Object.keys(packageJSON.optionalDependencies),
 		"util"
 	]
 };
