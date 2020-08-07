11d8c5d063e8b856024dd04137f6c3047ca92339
wessberg <frederikwessberg@gmail.com>
2020-08-08 01:54:12 +0200

    chore(build): fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -24,7 +24,7 @@ export default {
 	],
 	external: [
 		...Object.keys(packageJSON.dependencies),
-		...Object.keys(packageJSON.devDependencies)
+		...Object.keys(packageJSON.devDependencies),
 		"util", "path"
 	]
 };
